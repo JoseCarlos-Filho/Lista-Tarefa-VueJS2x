882918241abb91995c8e05a09b03ca13b11336da
José Carlos <etijcvivi@gmail.com>
2022-06-30 13:26:06 -0300

    Projeto rodando. Finalizado utilizaçao e implementação do localStorage em App.vue

--- src/App.vue
+++ src/App.vue
@@ -27,15 +27,28 @@ export default {
   },
   computed: { 
     progress() {
-      const total = this.tasks.length;
-      const done = this.tasks.filter(t => !t.pending).length;
-      return Math.round(done / total * 100) || 0;
+      const total = this.tasks.length; // pega o tamanho do array
+      const done = this.tasks.filter(t => !t.pending).length; // através da função filter verifica se esta pendente ou feito.
+      return Math.round(done / total * 100) || 0; // retorna o total de tarefas feitas.
     }
   },
-  watch: { 
-    tasks() {
-      localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    }
+  // Assistindo as tasks geradas no array, gerando uma string em JSON.
+  watch: {
+    // faz um watch profundo, tasks agora é um objeto, ollhando de forma mais profunda
+    // os itens no array neste caso olhando cada tasks e os valores de seus atributos
+    // olhando so estado de pendente ou não. salvando no localStorage além dos itens,
+    // mais também o estado atual da tarefa. 
+    tasks: { 
+        deep: true,
+        handler() {
+          localStorage.setItem("tasks", JSON.stringify(this.tasks));
+        }
+    } 
+    // tasks() {
+    //   // digite o comando no console: localStore.setItem('tasks')
+    //   // para vizualizar as tarefas adicionadas no array e seus atributos.
+    //   localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    // }
   },
 
   methods: {
@@ -52,12 +65,28 @@ export default {
     },
 
     tarefaApaga(indice) {
+      // utiliza a função splice onde passo o indice do array 
+      // e quantidade de item, no caso 1 item.
       this.tasks.splice(indice, 1);
     },
 
     marcaEstadoTarefa(indice) {
       this.tasks[indice].pending = !this.tasks[indice].pending;
     }
+  },
+  // metodo de ciclo de vida para ler as informações do localStorage
+  created() {
+    const json = localStorage.getItem('tasks')
+    const taskArray = JSON.parse(json)
+    // condição que verifica taskArray se de fato é um array caso contrario é um array vazio.
+    // No caso a solução aplicada abaixo com operador ternário.
+    //  condição de if e else tambem funciona para este tipo de checagem.
+    this.tasks = Array.isArray(taskArray) ? taskArray : []
+    // if(Array.isArray(taskArray)) {
+    //   this.tasks = taskArray;
+    // } else {
+    //   this.tasks =[]
+    // }
   }
 }
 </script>
